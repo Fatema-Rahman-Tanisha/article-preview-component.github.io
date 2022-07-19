c677d53145116fca52d846850f0e81bc1518d994
Fatema <syedaevanahossain@gmail.com>
2022-07-19 21:58:31 +0600

    initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+shareButton= document.getElementById('shareButton');
+shareButton.addEventListener('click',()=>{
+    shareDiv.classList.toggle('hidden')
+    shareButton.classList.toggle('translate-y-[-20px]')
+    // shareButton.classList.toggle('translate-y-[-20px]')
+})
